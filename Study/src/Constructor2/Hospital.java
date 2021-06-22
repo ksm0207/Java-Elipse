46601fa937fcb54f9ddab1794beeaf5d8c5063c6
ksm0207 <ksm03071@naver.com>
2021-06-22 16:51:01 +0900

    Update Hospital.java

--- Study/src/Constructor2/Hospital.java
+++ Study/src/Constructor2/Hospital.java
@@ -5,6 +5,7 @@ public class Hospital {
 	public static void main(String[] args) {
 		
 		Person p1 = new Person("±è¼º¹Î","Áøµ¾°³","´Ù¸®¾ÆÇÄ");
+		Person p2 = new Person("Kim","Rabit","???");
 		
 		System.out.print(p1);
 	}
