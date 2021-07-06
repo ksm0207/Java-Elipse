887fa25dd50098527d201fcf89890b107c90af1b
ksm0207 <ksm03071@naver.com>
2021-07-06 14:20:55 +0900

    Update Gugudan.java

--- Study/src/Math/Gugudan.java
+++ Study/src/Math/Gugudan.java
@@ -7,7 +7,7 @@ public class Gugudan {
 	int N = 0;
 
 	public void Result(int N) {
-		System.out.print("구구단 시작\n");
+		
 		System.out.print(N + " 단 을 출력합니다.\n");
 		for (int i = 1 ; i < 10 ; i ++) {
 			System.out.print(N + " x " + i + " = " + N * i +"\n");
@@ -24,7 +24,7 @@ public class Gugudan {
 		
 		Gugudan gd = new Gugudan();
 		Scanner scan = new Scanner(System.in);
-		
+		System.out.print("구구단 시작 : ");
 		int setValue = scan.nextInt();
 		gd.Result(setValue);
 	}
