0a05225190e790e21951f4c8b564eff0c99fb413
ksm0207 <ksm03071@naver.com>
2021-07-06 11:23:56 +0900

    Create Gugudan.java

---
+++ Study/src/Math/Gugudan.java
@@ -0,0 +1,32 @@
+package Math;
+
+import java.util.Scanner;
+
+public class Gugudan {
+	
+	int N = 0;
+
+	public void Result(int N) {
+		System.out.print("구구단 시작\n");
+		System.out.print(N + " 단 을 출력합니다.\n");
+		for (int i = 1 ; i < 10 ; i ++) {
+			System.out.print(N + " x " + i + " = " + N * i +"\n");
+//			for (int j = 1 ; j < 10 ; j ++ ) {
+			// Not Input
+//				
+//				System.out.print(i + " x " + j + " = " + (i * j) +"\n");
+//			}
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		Gugudan gd = new Gugudan();
+		Scanner scan = new Scanner(System.in);
+		
+		int setValue = scan.nextInt();
+		gd.Result(setValue);
+	}
+
+}
